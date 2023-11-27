b7650ffbb3f82d1b59c09e6d84ca7e485564a1ae
Azamat-Sharipov <navom65@gmail.com>
2023-11-27 09:31:48 -0800

    frist

---
+++ main.py
@@ -0,0 +1,80 @@
+print("Hello World") # Mattini kansolga chiqarish
+#
+# print(2 + 3) # Koshish +
+# print(2 - 3) # Ayirish -
+# print(-2 - 3) # Ayirish -
+# print(-2 * 3) # Kopaytirish *
+# print(3 / 2) # Toza-Bolish /
+# print(3 // 2) # Bolish (Butun qisim qoladi) /
+# print(3 % 2) # Qoldiq %
+
+# print("Hello World - 2")# O'zgaruvchilar
+
+# number = 2 # Quti
+# Azamat = "Azamat"
+# print(Azamat)
+
+# print("Hello World - 3")# Ma'lumotlar turlari
+
+# print(12) # Integer - bular sonnlar - cheksizlikdan + cheksizlikkacha
+# print("Hello") # String - bular qo'shtirnoq ichiga yoziladi
+# print(2 > 3) # Booling - bular < - > True, Folse
+
+# Taqqoslash
+# print(2 > 3)
+# print(3 < 2)
+# print(5 < 6)
+# print(123 < 123)
+# Qatiy teng
+# print(5 == 5)
+# print(5 >= 5)
+# print(5 >= 5)
+# Qatiy teng
+
+# Qatiy teng emas
+# print(5 != 3)
+# print(5 != 5)
+# Qatiy teng emas
+
+# Stringa + Stringni Qoshsih...
+# a = input("Ismingizni kiriting: ")
+# b = input("Yoshingizni kiriting: ")
+# c = input("Viloyat: ")
+# d = input("Manzilingiz: ")
+# print("Ism: " + a)
+# print("Yosh: " + b)
+# print("Oblest: " + c)
+# print("Manzil: " + d)
+
+# Integerni + Integerga Qoshish...
+# X = int(input("x: "))
+# Y = int(input("y: "))
+# print("Natija:", X * Y)
+
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+#
+# print("Natija: ", a * b)
+#
+# a = "kometa: "
+# b = 3
+# d = a * b
+# print(d)
+
+# 5 ning - 4 darajasi... 1-misol
+a = int(input("daraja: "))
+print("natija: ", a ** 4)
+
+# Qoldiq... 2-misol
+a = int(input("Reaqam kiriting: "))
+c = 22 % a
+print("Natija: ", c)
+
+# Kvadratning yuzi va perimetiri... 3-misol
+a = int(input("A tomonini kiriting: "))
+b = int(input("B tomonini kiriting: "))
+c = int(input("C tomonini kiriting: "))
+d = int(input("D tomonini kiriting: "))
+print("X: ", a * c)
+print("Y: ", b * d)
+
